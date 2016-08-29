52f77d5dbd31b72ca5f26b4ea6bfee72ad593911
Chris Aporta <christopher.aporta@gmail.com>
2016-08-28 23:04:16 -0400

    create functioning repl leveraging babel-standalone for on-the-fly transpiling in the browser. provide state + error handling.

--- App.js
+++ App.js
@@ -1,6 +1,40 @@
 import React from 'react';
 
 
-const App = () => <div>Hello World!</div>;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            placeholder: 'Enter your ES6+ here!',
+            output: '',
+            err: ''
+        }
+        this.update = this.update.bind(this);
+    }
+    render() {
+        return(
+            <div>
+                <header>{this.state.err}</header>
+                <div>
+                    <textarea onChange={this.update} defaultValue={this.state.placeholder}></textarea>
+                    <pre>{this.state.output}</pre>
+                </div>
+            </div>
+        );
+    }
+    update(el) {
+        let input = el.target.value;
+        try {
+            this.setState({
+                output: Babel.transform(input, {
+                    presets: ['es2015', 'react']
+                }).code,
+                err: ''
+            });
+        } catch (err) {
+            this.setState({err: err.message});
+        }
+    }
+}
 
 export default App;
